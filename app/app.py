e0d4d3f6581dfe9357b889f27a428813e2bfaf27
翁治平 <gocreating@gmail.com>
2019-07-21 23:54:53 +0800

    Update

--- app/app.py
+++ app/app.py
@@ -1,29 +1,32 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, Blueprint, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, subdomain_matching=True)
 app.config['SERVER_NAME'] = 'cubee.cc'
 
+#########
+## App ##
+#########
 @app.route('/')
-def index():
+def app_index():
+    return 'app index'
+
+#########
+## WWW ##
+#########
+www = Blueprint('www', __name__, subdomain='www')
+@www.route('/')
+def www_index():
     return 'Welcome to Cubee API Server'
 
-@app.route('/hello')
+@www.route('/hello')
 def hello():
     return jsonify(
         hello='world',
         num=777,
     )
 
-@app.route('/test', subdomain="<username>")
-def username_test(username):
-    return 'Hi, {0}'.format(username)
-
-@app.route('/', subdomain="<username>")
-def username_index(username):
-    return 'Fuck, {0}'.format(username)
-
-@app.route('/version')
+@www.route('/version')
 def version():
     try:
         return jsonify(
@@ -32,6 +35,19 @@ def version():
         )
     except:
         return 'unknown'
+
+app.register_blueprint(www)
+
+#############
+## Dynamic ##
+#############
+@app.route('/test', subdomain="<username>")
+def username_test(username):
+    return 'Hi, {0}'.format(username)
+
+@app.route('/', subdomain="<username>")
+def username_index(username):
+    return 'Fuck, {0}'.format(username)
 
 if __name__ == '__main__':
     app.run()
